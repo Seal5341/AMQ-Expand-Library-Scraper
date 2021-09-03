3858aa9836d98382566441fb922dbd611ecd7d49
Seal5341 <90068441+Seal5341@users.noreply.github.com>
2021-09-04 04:44:05 +0800

    Add files via upload

---
+++ amqscrape.py
@@ -0,0 +1,251 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Seal
+
+"""
+
+import csv
+import traceback
+import time
+from datetime import datetime
+from userpass import userpass_combo
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, ElementNotInteractableException
+# from selenium.webdriver.common.action_chains import ActionChains
+
+filename = '20210810.csv'
+
+count = 0
+prev_link = ''
+old_anime_name = ''
+lengthdict = {}
+
+print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 'Starting run')
+starttime = time.time()
+
+# start web browser
+driver = webdriver.Firefox() # firefox is faster than chrome and edge, not sure if headless works
+driver.maximize_window()
+# action = ActionChains(driver)
+
+# login - enter your own username and password
+driver.get("https://animemusicquiz.com/")
+loginUser = driver.find_element_by_id("loginUsername")
+loginUser.send_keys(userpass_combo()[0])
+loginPass = driver.find_element_by_id("loginPassword")
+loginPass.send_keys(userpass_combo()[1])
+loginPass.send_keys(Keys.RETURN)
+
+# expandlibrary
+print('Waiting for menu to load...')
+expandbutton = WebDriverWait(driver, 9999).until(EC.presence_of_element_located((By.ID,'mpExpandButton')))
+loading_screen = driver.find_element_by_id('loadingScreen')
+WebDriverWait(driver, 9999).until(EC.invisibility_of_element_located(loading_screen))
+cookie = driver.find_element_by_class_name('cc-compliance')
+cookie.click()
+expandbutton.click()
+print('Waiting for expand library to load...')
+expand = WebDriverWait(driver, 9999).until(EC.presence_of_all_elements_located((By.CLASS_NAME,'elQuestion')))
+totalnumberofentries = len(expand) - 1 # first entry is a dummy
+lengthdict[0] = totalnumberofentries
+print('Expand library loaded with', totalnumberofentries, 'anime')
+
+# open file for saving data
+file = open(filename, 'w', newline='', encoding='utf-8')
+file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+file_writer.writerow(['Anime Name', 'Song Type', 'Song Name', 'Song Artist', 'Mp3 Link', 'Webm Link'])
+
+# calls to driver
+mp3button = driver.find_element_by_id('elSelectorOptionMp3')
+inputvideo = driver.find_element_by_id('elInputVideo')
+
+# iterate through anime list
+try:
+    for elQuestion in expand[1:]: # first entry is a dummy
+        try:
+            # open anime box
+            driver.execute_script('arguments[0].scrollIntoView();', elQuestion)
+            anime_name = elQuestion.find_element_by_xpath('.//div[@class="elQuestionAnimeName"]').text
+            elQuestion.click()
+            
+            # song box
+            songinfos = elQuestion.find_elements_by_xpath('.//div[@class="elQuestionSongInfo"]')
+            
+            linecontainer = []
+            for song_info in songinfos:
+                
+                # click on song
+                driver.execute_script("arguments[0].scrollIntoView();", song_info)
+                song_info.click()
+                
+                webm_link = inputvideo.get_attribute('src').split('/')[-1]
+                if webm_link != prev_link:
+                    prev_link = webm_link
+                else:
+                    webm_link = 'Not Found'
+                
+                try:
+                    mp3button.click() # element not interactable error if doesnt exist
+                    mp3_link = inputvideo.get_attribute('src').split('/')[-1]
+                    if mp3_link != prev_link:
+                        prev_link = mp3_link
+                    else:
+                        mp3_link = 'Not Found'
+                except:
+                    mp3_link = 'Not Found'
+                
+                # get song info and save
+                song_type = song_info.find_element_by_xpath('.//div[@class="elQuestionSongType"]').text
+                song_name = song_info.find_element_by_xpath('.//div[@class="elQuestionSongName"]').text
+                song_artist = song_info.find_element_by_xpath('.//div[@class="elQuestionSongArtist"]').text
+                
+                linecontainer.append([anime_name, song_type, song_name, song_artist, mp3_link, webm_link])
+            
+            # click to close anime box
+            # driver.execute_script('arguments[0].scrollIntoView();', elQuestion)
+            # elQuestion.click()
+            
+            for ln in linecontainer:
+                file_writer.writerow(ln)
+            
+            count += 1
+            old_anime_name = anime_name
+            if count%10 == 0:
+                print('Anime #' + str(count) + ':', linecontainer[-1])
+        
+        except NoSuchElementException: # for first elQuestion object
+            print('Element not found')
+            continue
+except ElementClickInterceptedException:
+    print('Click intercepted, restarting browser')
+except ElementNotInteractableException:
+    print('Scroll intercepted, restarting browser')
+except:
+    traceback.print_exc()
+
+file.close() # close periodically to save data
+driver.quit()
+
+print('Initial run complete, running loop now')
+runcount = 1
+
+# iterate through anime list
+while count < totalnumberofentries:
+    # open file for saving data
+    file = open(filename, 'a+', newline='', encoding='utf-8')
+    file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    # file_writer.writerow(['Anime Name', 'Song Type', 'Song Name', 'Song Artist', 'Mp3 Link', 'Webm Link'])
+    
+    print('Waiting for auto logout...\n')
+    time.sleep(30) # dodge login confirmation due to imcomplete logout
+    print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 'Loop #' + str(runcount))
+    print('count =', count)
+    
+    # start web browser
+    driver = webdriver.Firefox() # firefox is faster than chrome and edge, not sure if headless works
+    driver.maximize_window()
+    # action = ActionChains(driver)
+    
+    try:
+        # login - enter your own username and password
+        driver.get("https://animemusicquiz.com/")
+        loginUser = driver.find_element_by_id("loginUsername")
+        loginUser.send_keys(userpass_combo()[0])
+        loginPass = driver.find_element_by_id("loginPassword")
+        loginPass.send_keys(userpass_combo()[1])
+        loginPass.send_keys(Keys.RETURN)
+        
+        # expandlibrary
+        print('Waiting for menu to load...')
+        expandbutton = WebDriverWait(driver, 9999).until(EC.presence_of_element_located((By.ID,'mpExpandButton')))
+        loading_screen = driver.find_element_by_id('loadingScreen')
+        WebDriverWait(driver, 9999).until(EC.invisibility_of_element_located(loading_screen))
+        cookie = driver.find_element_by_class_name('cc-compliance')
+        cookie.click()
+        expandbutton.click()
+        print('Waiting for expand library to load...')
+        expand = WebDriverWait(driver, 9999).until(EC.presence_of_all_elements_located((By.CLASS_NAME,'elQuestion')))
+        totalentries = len(expand) - 1
+        lengthdict[runcount] = totalentries
+        print('Expand library loaded with', totalentries, 'anime') # first entry is a dummy
+        
+        # calls to driver
+        mp3button = driver.find_element_by_id('elSelectorOptionMp3')
+        inputvideo = driver.find_element_by_id('elInputVideo')
+        
+        for elQuestion in expand[count+1:]:
+            # open anime box
+            driver.execute_script('arguments[0].scrollIntoView();', elQuestion)
+            anime_name = elQuestion.find_element_by_xpath('.//div[@class="elQuestionAnimeName"]').text
+            elQuestion.click()
+            
+            # song box
+            songinfos = elQuestion.find_elements_by_xpath('.//div[@class="elQuestionSongInfo"]')
+            
+            linecontainer = []
+            for song_info in songinfos:
+                
+                # click on song
+                driver.execute_script("arguments[0].scrollIntoView();", song_info)
+                song_info.click()
+                
+                webm_link = inputvideo.get_attribute('src').split('/')[-1]
+                if webm_link != prev_link:
+                    prev_link = webm_link
+                else:
+                    webm_link = 'Not Found'
+                
+                try:
+                    mp3button.click() # element not interactable error if doesnt exist
+                    mp3_link = inputvideo.get_attribute('src').split('/')[-1]
+                    if mp3_link != prev_link:
+                        prev_link = mp3_link
+                    else:
+                        mp3_link = 'Not Found'
+                except:
+                    mp3_link = 'Not Found'
+                
+                # get song info and save
+                song_type = song_info.find_element_by_xpath('.//div[@class="elQuestionSongType"]').text
+                song_name = song_info.find_element_by_xpath('.//div[@class="elQuestionSongName"]').text
+                song_artist = song_info.find_element_by_xpath('.//div[@class="elQuestionSongArtist"]').text
+                
+                linecontainer.append([anime_name, song_type, song_name, song_artist, mp3_link, webm_link])
+            
+            # click to close anime box
+            # driver.execute_script('arguments[0].scrollIntoView();', elQuestion)
+            # elQuestion.click()
+            
+            for ln in linecontainer:
+                file_writer.writerow(ln)
+            
+            count += 1
+            old_anime_name = anime_name
+            if count%10 == 0:
+                print('Anime #' + str(count) + ':', linecontainer[-1])
+            
+    except ElementClickInterceptedException:
+        print('Click intercepted, restarting browser')
+    except ElementNotInteractableException:
+        print('Scroll intercepted, restarting browser')
+    except KeyboardInterrupt:
+        print('Keyboard Interrupt')
+        file.close()
+        break
+    except:
+        traceback.print_exc()
+    
+    print('Last Anime: #' + str(count), old_anime_name)
+    file.close() # close periodically to save data
+    driver.quit()
+    runcount += 1
+
+file.close()
+timetaken = time.time() - starttime
+print('Time taken:', timetaken/3600, 'hours')
+print('Total anime scraped:', count)
+print(lengthdict)
---
+++ readdatabasegui.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 15 11:15:21 2021
+
+@author: Seal
+"""
+
+import pandas as pd
+import tkinter as tk
+
+csvfile = 'combined.csv'
+
+df = pd.read_csv(csvfile,encoding='utf-8')
+
+window = tk.Tk()
+window.title('Read AMQ Database GUI')
+window.geometry('+1096+137')
+window.resizable(True,True)
+window.attributes('-topmost',True)
+
+output_anime = tk.Label(window, text='Anime')
+output_song = tk.Label(window, text='Song')
+output_artist = tk.Label(window, text='Artist')
+
+def getsonginfo(event='event'):
+    filename = searchinput.get().split('/')[-1]
+    search.configure(text='Search: ' + filename)
+    songname, artistname = '', ''
+    global output_anime, output_song, output_artist
+    try:
+        if filename.split('.')[-1] == 'webm':
+            selection = df.loc[df['Webm Link'] == filename]
+        else:
+            if filename.split('.')[-1] != 'mp3':
+                filename += '.mp3'
+            selection = df.loc[df['Mp3 Link'] == filename]
+        animename = str(selection['Anime Name'].values[0])
+        songtype = str(selection['Song Type'].values[0])
+        songname = str(selection['Song Name'].values[0])
+        artistname = str(selection['Song Artist'].values[0])
+        window.clipboard_clear()
+        window.clipboard_append(animename)
+    except:
+        animename = 'Not Found'
+        songtype = ''
+        songname = 'Not Found'
+        artistname = 'Not Found'
+    output_anime.configure(text=animename + ' ' + songtype)
+    output_song.configure(text='Song: ' + songname)
+    output_artist.configure(text='Artist: ' + artistname)
+    samesongartist(animename,songtype,songname,artistname,filename,filename.split('.')[-1])
+    searchinput.delete('0', 'end')
+
+def copysongtoclipboard():
+    window.clipboard_clear()
+    window.clipboard_append('/as ')
+    window.clipboard_append(output_song['text'].split(' ')[1:])
+
+def copyartisttoclipboard():
+    window.clipboard_clear()
+    window.clipboard_append('/aa ')
+    window.clipboard_append(output_artist['text'].split(' ')[1:])
+    
+def copyall():
+    window.clipboard_clear()
+    window.clipboard_append('a/ ')
+    window.clipboard_append(output_anime['text'].split(' ')[:-1])
+    window.clipboard_append(' | ')
+    window.clipboard_append(output_song['text'].split(' ')[1:])
+    window.clipboard_append(' | ')
+    window.clipboard_append(output_artist['text'].split(' ')[1:])
+
+def samesongartist(animename,songtype,songname,artistname,filename,filetype):
+    dfsongartist = pd.DataFrame()
+    try:
+        if filetype == 'mp3':
+            column = 'Mp3 Link'
+        elif filetype == 'webm':
+            column = 'Webm Link'
+        dfsongartist = df.loc[df['Anime Name'] != animename].loc[df['Song Name'] == songname].loc[df['Song Artist'] == artistname].loc[df[column] != filename]
+    except:
+        pass
+    if len(dfsongartist) == 0:
+        txt = 'No Dupe Found'
+    else:
+        txt = 'Dupes:'
+        for i in range(len(dfsongartist)):
+            txt += '\n' + dfsongartist['Anime Name'].values[i] + ' ' + dfsongartist['Song Type'].values[i]
+    dupeanime1.configure(text=txt)
+
+def frosty():
+    window.clipboard_clear()
+    window.clipboard_append(':muscle: 𐌅Ṝ๏𝓼Ƭץ :muscle:')
+
+frosty_button = tk.Button(text='💪𐌅Ṝ๏𝓼Ƭץ💪', command=frosty)
+frosty_button.grid(column=0, row=0)
+
+button1 = tk.Button(text='Get Song Info', command=getsonginfo)
+button3 = tk.Button(text='Copy Song to Clipboard', command=copysongtoclipboard)
+button4 = tk.Button(text='Copy Artist to Clipboard', command=copyartisttoclipboard)
+button5 = tk.Button(text='Copy All Metadata', command=copyall)
+
+search = tk.Label(window, text='Search')
+search.grid(column=0, row=1)
+searchinput = tk.Entry(window)
+searchinput.bind('<Return>', getsonginfo)
+searchinput.grid(column=0, row=2)
+button1.grid(column=0, row=3)
+
+output_anime.grid(column=0, row=4)
+output_song.grid(column=0, row=5)
+output_artist.grid(column=0, row=6)
+button3.grid(column=0, row=7)
+button4.grid(column=0, row=8)
+button5.grid(column=0, row=9)
+
+dupeanime1 = tk.Label(window,text='Dupes')
+dupeanime1.grid(column=0, row=10, rowspan=3)
+
+def addsong():
+    global df
+    animename = animenameinput.get()
+    songtype = songtypeinput.get()
+    songname = songnameinput.get()
+    songartist = songartistinput.get()
+    mp3link = mp3linkinput.get().split('/')[-1]
+    webmlink = webmlinkinput.get().split('/')[-1]
+    new_row = {'Anime Name':animename, 'Song Type':songtype, 'Song Name':songname, 'Song Artist': songartist, 'Mp3 Link': mp3link, 'Webm Link': webmlink}
+    df = df.append(new_row, ignore_index=True)
+    df.to_csv(csvfile,index=False)
+    animenameinput.delete('0', 'end')
+    songtypeinput.delete('0', 'end')
+    songnameinput.delete('0', 'end')
+    songartistinput.delete('0', 'end')
+    mp3linkinput.delete('0', 'end')
+    webmlinkinput.delete('0', 'end')
+
+tk.Label(window, text='Anime Name').grid(column=1, row=0)
+animenameinput = tk.Entry(window)
+animenameinput.grid(column=1, row=1)
+tk.Label(window, text='Song Type').grid(column=1, row=2)
+songtypeinput = tk.Entry(window)
+songtypeinput.grid(column=1, row=3)
+tk.Label(window, text='Song Name').grid(column=1, row=4)
+songnameinput = tk.Entry(window)
+songnameinput.grid(column=1, row=5)
+tk.Label(window, text='Song Artist').grid(column=1, row=6)
+songartistinput = tk.Entry(window)
+songartistinput.grid(column=1, row=7)
+tk.Label(window, text='Mp3 Link').grid(column=1, row=8)
+mp3linkinput = tk.Entry(window)
+mp3linkinput.grid(column=1, row=9)
+tk.Label(window, text='Webm Link').grid(column=1, row=10)
+webmlinkinput = tk.Entry(window)
+webmlinkinput.grid(column=1, row=11)
+addsongbutton = tk.Button(text='Add Song', command=addsong).grid(column=1, row=12)
+
+window.mainloop()
